c62cf992348acc610ceb1abc822eaa58b8e4a7de
anshaj <anshaj@slanglabs.in>
2019-01-10 17:31:19 +0530

    UI Fixes

--- app/src/main/java/com/example/akanshisrivastava/bankingapp/MoneyTransfer.java
+++ app/src/main/java/com/example/akanshisrivastava/bankingapp/MoneyTransfer.java
@@ -3,18 +3,28 @@ package com.example.akanshisrivastava.bankingapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.akanshisrivastava.bankingapp.adapters.NothingSelectedSpinnerAdapter;
 
 public class MoneyTransfer extends AppCompatActivity {
 
     Button imps, neft, proceed;
     LinearLayout neftDescription;
     TextView description;
+    EditText amount, remarks;
+    Spinner payeeSpinner;
     private static final String TAG = MoneyTransfer.class.getSimpleName();
 
     @Override
@@ -25,6 +35,43 @@ public class MoneyTransfer extends AppCompatActivity {
 
         neftDescription = findViewById(R.id.neft_layout);
         description = findViewById(R.id.type_description);
+
+        amount = findViewById(R.id.transfer_amount);
+        remarks = findViewById(R.id.transfer_remarks);
+
+        amount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                enableSubmitIfReady();
+            }
+        });
+
+        remarks.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                enableSubmitIfReady();
+            }
+        });
 
         imps = findViewById(R.id.imps);
         imps.setFocusable(true);
@@ -68,11 +115,32 @@ public class MoneyTransfer extends AppCompatActivity {
         });
 
         proceed = findViewById(R.id.proceed);
+        proceed.setEnabled(false);
         proceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MoneyTransfer.this, "Your funds will be transferred", Toast.LENGTH_LONG).show();
             }
         });
+
+        payeeSpinner = findViewById(R.id.payee_spinner);
+
+        ArrayAdapter<CharSequence> payeeAdapter =
+                ArrayAdapter.createFromResource(this, R.array.transfer_list, android.R.layout.simple_spinner_item);
+        payeeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        payeeSpinner.setAdapter(new NothingSelectedSpinnerAdapter(
+                payeeAdapter,
+                R.layout.spinner_row_nothing_selected_payee,
+                this
+        ));
+
+
+    }
+
+    private void enableSubmitIfReady() {
+        boolean isReady = !amount.getText().toString().isEmpty() &&
+                !remarks.getText().toString().isEmpty();
+        proceed.setEnabled(isReady);
     }
 }
